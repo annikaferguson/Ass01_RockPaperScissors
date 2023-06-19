f9f6b64bfe474afcf43a49822024a754b9615d84
annikaferguson <fergusak@mail.uc.edu>
2023-06-19 16:35:45 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -7,53 +7,45 @@ public class Main {
         String playerB = "Player B";
         String yesOrNo = "";
 
-        do
-        {
+        do {
             System.out.println("Would you like to play Rock Paper Scissors? Yes or No?");
-            yesOrNo = in.nextLine();
+            if (yesOrNo == in.nextLine()) {
+                do {
+                    System.out.println("Player A: choose R, P, or S");
+                    playerA = in.nextLine();
+                    System.out.println("Player B: choose R, P, or S");
+                    playerB = in.nextLine();
 
-            do
-            {
-                System.out.println("Player A: choose R, P, or S");
-                playerA = in.nextLine();
-                System.out.println("Player B: choose R, P, or S");
-                playerB = in.nextLine();
-
-                if(playerA.equalsIgnoreCase("R"))
-                {
-                    if(playerB.equalsIgnoreCase("R"))
-                    {
-                        System.out.println("Rock vs. Rock. It's a tie!");
-                    } else if(playerB.equalsIgnoreCase("P")) {
-                        System.out.println("Paper beats Rock! Player B wins!");
-                    } else {
-                        System.out.println("Rock beats Scissors! Player A wins!");
-                    }
-                } else if(playerA.equalsIgnoreCase("P"))
-                {
-                    if(playerB.equalsIgnoreCase("R"))
-                    {
-                        System.out.println("Paper beats Rock! Player A wins!");
-                    } else if(playerB.equalsIgnoreCase("P")) {
-                        System.out.println("Paper vs. Paper. It's a tie!");
-                    } else {
-                        System.out.println("Scissors beats Paper! Player B wins!");
-                    }
-                } else {
-                    if(playerB.equalsIgnoreCase("R"))
-                    {
+                    if (playerA.equalsIgnoreCase("R")) {
+                        if (playerB.equalsIgnoreCase("R")) {
+                            System.out.println("Rock vs. Rock. It's a tie!");
+                        } else if (playerB.equalsIgnoreCase("P")) {
+                            System.out.println("Paper beats Rock! Player B wins!");
+                        } else {
+                            System.out.println("Rock beats Scissors! Player A wins!");
+                        }
+                    } else if (playerA.equalsIgnoreCase("P")) {
+                        if (playerB.equalsIgnoreCase("R")) {
+                            System.out.println("Paper beats Rock! Player A wins!");
+                        } else if (playerB.equalsIgnoreCase("P")) {
+                            System.out.println("Paper vs. Paper. It's a tie!");
+                        } else {
+                            System.out.println("Scissors beats Paper! Player B wins!");
+                        }
+                    } else if (playerB.equalsIgnoreCase("R")) {
                         System.out.println("Rock beats Scissors! Player B wins!");
-                    } else if(playerB.equalsIgnoreCase("P")) {
+                    } else if (playerB.equalsIgnoreCase("P")) {
                         System.out.println("Scissors beats Paper! Player A wins!");
                     } else {
                         System.out.println("Scissors vs. Scissors! It's a tie!");
+
+                        System.out.println("Would you like to play again? Yes or No?");
+                        yesOrNo = in.nextLine();
                     }
-                }
-
-                System.out.println("Would you like to play again? Yes or No?");
-                yesOrNo = in.nextLine();
-
-            } while (yesOrNo.equalsIgnoreCase("Yes"))
-        }
+                } while (yesOrNo.equalsIgnoreCase("Yes"));
+            } else {
+                System.out.println("Invalid input. Please enter yes or no.");
+            }
+        } while (yesOrNo.equalsIgnoreCase("Yes"));
     }
 }
